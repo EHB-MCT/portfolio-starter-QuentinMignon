0af4b69b698859fe96c605a3f3662d8638cfab87
Quentin Mignon <quentin.mignon@student.ehb.be>
2023-10-12 11:04:23 +0200

    Added get players endpoint for my API

--- src/index.js
+++ src/index.js
@@ -10,3 +10,8 @@ app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
 
+app.get("/players", async (req, res) => {
+  const players = await knex("players").select();
+  res.json(players);
+});
+
