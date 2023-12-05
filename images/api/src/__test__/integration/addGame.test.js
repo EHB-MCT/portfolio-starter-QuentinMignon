97249b8e041b3364b195d02a393d6180f2703b61
Quentin Mignon <quentin.mignon@student.ehb.be>
2023-12-05 17:43:43 +0100

    Added integration test to add a game

---
+++ images/api/src/__test__/integration/addGame.test.js
@@ -0,0 +1,36 @@
+const knexConfig = require("./../../database/knexfile");
+const knex = require("knex")(knexConfig.development);
+const request = require("supertest");
+const app = require("./../../index");
+
+describe("POST /games", () => {
+  beforeAll(async () => {
+    await knex.raw("begin");
+  });
+
+  afterAll(async () => {
+    await knex("games").where("game", "TestGame").del();
+  });
+
+  test("Gives a success message when game is added", async () => {
+    const newGameData = {
+      game: "TestGame",
+      category: "testing",
+    };
+    const response = await request(app).post("/games").send(newGameData);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: "Game successfully added" });
+  });
+
+  test("Gives an error message when inputs are empty", async () => {
+    const newGameData = {
+      game: "",
+      category: "",
+    };
+    const response = await request(app).post("/games").send(newGameData);
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({
+      message: "Please fill in the missing fields",
+    });
+  });
+});
